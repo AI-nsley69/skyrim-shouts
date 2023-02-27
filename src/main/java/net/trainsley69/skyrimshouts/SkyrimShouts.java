ff75603d6fa286b1143e0a11708ad44a66ceb9ee
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2023-02-27 01:20:57 +0100

    Add initial class for Marked For Death status effect

--- src/main/java/net/trainsley69/skyrimshouts/SkyrimShouts.java
+++ src/main/java/net/trainsley69/skyrimshouts/SkyrimShouts.java
@@ -1,6 +1,13 @@
 package net.trainsley69.skyrimshouts;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.trainsley69.skyrimshouts.registry.effects.MarkedForDeath;
+import org.intellij.lang.annotations.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +17,20 @@ public class SkyrimShouts implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("skyrim-shouts");
 
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		Register();
 		LOGGER.info("Dovahkiin. Dragonborn. FUS RO DAH!");
+	}
+
+	// Registries
+	public static final MobEffect MFD = new MarkedForDeath();
+	public void Register() {
+		String name = "skyrimshouts";
+		Registry.register(BuiltInRegistries.MOB_EFFECT, new ResourceLocation(name, "Marked For Death"), MFD);
 	}
 }
---
+++ src/main/java/net/trainsley69/skyrimshouts/registry/effects/MarkedForDeath.java
@@ -0,0 +1,10 @@
+package net.trainsley69.skyrimshouts.registry.effects;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class MarkedForDeath extends MobEffect {
+    public MarkedForDeath() {
+        super(MobEffectCategory.HARMFUL, 0x5A247B);
+    }
+}
