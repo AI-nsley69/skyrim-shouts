dfee4a53a36bed4fa6c564f40d662ca55a4c4c72
Space Walker <spacedoesrs@gmail.com>
2023-03-01 00:51:49 +0100

    fix loom injected interfaces

--- src/main/java/net/trainsley69/skyrimshouts/input/InputHandler.java
+++ src/main/java/net/trainsley69/skyrimshouts/input/InputHandler.java
@@ -1,8 +1,8 @@
 package net.trainsley69.skyrimshouts.input;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
 
-import net.trainsley69.skyrimshouts.shouts.ShoutUser;
 import net.trainsley69.skyrimshouts.shouts.Shouts;
 
 public class InputHandler {
@@ -10,12 +10,16 @@ public class InputHandler {
     private static Minecraft minecraft = Minecraft.getInstance();
 
     public static void handleKeybinds() {
-        if (minecraft.player == null) return;
+        Player player = minecraft.player;
+
+        if (player == null) {
+            return;
+        }
 
         // TODO: do not hardcode
         while (KeyMappings.FIRST_SHOUT.consumeClick()) {
             // Cast here fixes "cannot find symbol method getShoutManager()" somehow
-            ((ShoutUser)minecraft.player).getShoutManager().use(Shouts.MARKED_FOR_DEATH);
+            player.getShoutManager().use(Shouts.MARKED_FOR_DEATH);
         }
     }
 }
